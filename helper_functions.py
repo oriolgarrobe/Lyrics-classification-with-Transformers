113871c840761190b49a844b15d2cc7bc2cf942d
oriolgarrobe <oriol.garrobe@gmail.com>
2020-12-04 10:06:28 +0100

    changed dataset

--- helper_functions.py
+++ helper_functions.py
@@ -1,18 +1,24 @@
 import pandas as pd
 
-def preprocess(df):
-    """Function that preprocesses a dataframe before feeding it to text classification libraries"""
+def preprocess(data):
+    """Function that preprocesses a dataframe before feeding it to text classification libraries.
+    Inputs:
+    - data. Dataframe containing text fields and gold classification labels-
     
-    # Filter English songs
-    df = df.loc[df['language'] == 'en']
+    Output:
+    - Dataframe with the text fields containing relevant information joint and the classes as integers.    
+    """
     
+    # Join relevant text info
+    data['text'] = data['headline'] + ' ' + data['short_description']
+
     # Filter relevant columns
-    df = df[['lyrics', 'playlist_genre']]
-    
+    data = data[['text', 'category']]
+
     # Change column names for model
-    df = df.rename(columns={'lyrics': 'text', 'playlist_genre': 'label'})
-    
+    data = data.rename(columns={'category': 'label'})
+
     # Lables str->numeric
-    df['label'] = pd.factorize(df['label'])[0]
+    data['label'] = pd.factorize(data['label'])[0]
     
-    return(df)
+    return(data)
